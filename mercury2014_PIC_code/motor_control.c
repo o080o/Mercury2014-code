c6329cccbde538de71befce4286dcf7d19930f19
Ben Jespersen <ben.j.jespersen@gmail.com>
2015-03-04 20:22:11 -0600

    Started updating motor controller code for 2 motors; added some pin definitions to header.

--- mercury2014_PIC_code/motor_control.c
+++ mercury2014_PIC_code/motor_control.c
@@ -3,27 +3,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int leadAHigh = 0b00000100;
-int leadALow  = 0b00001000;
-int leadBHigh = 0b00010000;
-int leadBLow  = 0b00100000;
-int leadCHigh = 0b01000000;
-int leadCLow  = 0b10000000;
+int leadAHighR = 0b00000100;
+int leadALowR  = 0b00001000;
+int leadBHighR = 0b00010000;
+int leadBLowR  = 0b00100000;
+int leadCHighR = 0b01000000;
+int leadCLowR  = 0b10000000;
+int leadAHighL = 0b000001;
+int leadALowL  = 0b000010;
+int leadBHighL = 0b000100;
+int leadBLowL  = 0b001000;
+int leadCHighL = 0b010000;
+int leadCLowL  = 0b100000;
 
-int bitmask;
-int phase1;
-int phase2;
-int phase3;
-
+int bitmaskR;
+int phase1R;
+int phase2R;
+int phase3R;
+int bitmaskL;
+int phase1L;
+int phase2L;
+int phase3L;
 
 void init_motors(void)
 {
-	bitmask = leadAHigh | leadALow | leadBHigh | leadBLow | leadCHigh | leadCLow;
-	TRISD = 0b00000000;//(TRISD | bitmask ) ^ bitmask ;
-	//bitmask = leadA | leadB | leadC;
-	phase1 = leadAHigh | leadBLow;
-	phase2 = leadBHigh | leadCLow;
-	phase3 = leadCHigh | leadALow;
+	bitmaskR = leadAHighR | leadALowR | leadBHighR | leadBLowR | leadCHighR | leadCLowR;
+	bitmaskL = leadAHighL | leadALowL | leadBHighL | leadBLowL | leadCHighL | leadCLowL;
+
+	TRISD = 0b00000000;//(TRISD | bitmaskR ) ^ bitmaskR ;
+	//bitmaskR = leadA | leadB | leadC;
+	phase1R = leadAHighR | leadBLowR;
+	phase2R = leadBHighR | leadCLowR;
+	phase3R = leadCHighR | leadALowR;
+
+	TRISA = ( TRISA | bitmaskL ) ^ bitmaskL;
+	phase1L = leadAHighL | leadBLowL;
+	phase2L = leadBHighL | leadCLowL;
+	phase3L = leadCHighL | leadALowL;
 }
 
 
@@ -46,15 +62,20 @@ void motor_controller_test(void)
 */
 }
 
-void phase( int p )
+void phase( int p, char motor )
 {
 	p = p % 3;
-	if ( p==0 ){
-		PORTD = ( (PORTD | bitmask) ^ bitmask ) | phase1;
-	} else if ( p==1 ){
-		PORTD = ( (PORTD | bitmask) ^ bitmask ) | phase2;
-	} else if ( p==2 ) {
-		PORTD = ( (PORTD | bitmask) ^ bitmask ) | phase3;
+	if (motor == 0) {
+		if ( p==0 ){
+			PORTD = ( (PORTD | bitmaskR) ^ bitmaskR ) | phase1R;
+		} else if ( p==1 ){
+			PORTD = ( (PORTD | bitmaskR) ^ bitmaskR ) | phase2R;
+		} else if ( p==2 ) {
+			PORTD = ( (PORTD | bitmaskR) ^ bitmaskR ) | phase3R;
+		}
+	}
+	else if (motor == 1) {
+		
 	}
 }
 
--- mercury2014_PIC_code/prototypes.h
+++ mercury2014_PIC_code/prototypes.h
@@ -2,6 +2,21 @@
 #define _XTAL_FREQ 2457600
 #endif
 
+// Pin definitions (PIC 1 - Motor control)
+#define __A_HIGH_1		RD2
+#define __A_LOW_1		RD3
+#define __B_HIGH_1		RD4
+#define __B_LOW_1		RD5
+#define __C_HIGH_1		RD6
+#define __C_LOW_1		RD7
+
+#define __A_HIGH_2		RA0
+#define __A_LOW_2		RA1
+#define __B_HIGH_2		RA2
+#define __B_LOW_2		RA3
+#define __C_HIGH_2		RA4
+#define __C_LOW_2		RA5
+
 // Defined in main.c
 void init(void);
 
